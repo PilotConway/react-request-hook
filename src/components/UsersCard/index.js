817d4d72470e67b457c25edc1f271071af00cc2f
John Conway <john.conway@mcmurdo.orolia.com>
2019-07-17 13:04:14 -0400

    Removed components and moved to index.js that exports client

--- src/components/UsersCard/index.js
+++
@@ -1,2 +0,0 @@
-export { default } from './UsersCard';
-export { default as UsersList } from './UsersList';
---
+++ src/index.js
@@ -0,0 +1,4 @@
+export { createClient } from './client';
+export { default as ClientProvider } from './ClientProvider';
+export { default as Request } from './Request';
+export { default as useEndpointData } from './useEndpointData';
